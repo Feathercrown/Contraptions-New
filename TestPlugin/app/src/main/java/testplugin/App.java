12dddf79e772670032ddda20f33544a6af8b2abb
Feathercrown <gabriel.buonomano@gmail.com>
2024-02-19 21:10:28 -0500

    First commit

---
+++ TestPlugin/app/src/main/java/testplugin/App.java
@@ -0,0 +1,26 @@
+package testplugin;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin implements Listener {
+
+    public String getGreeting() {
+        return "hi";
+    }
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+
+}
